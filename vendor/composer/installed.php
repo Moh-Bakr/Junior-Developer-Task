685643bedff50ae16f4468db3d9c524470c7e29b
Mohamed Bakr <moh.bakr27@gmail.com>
2022-07-01 20:21:39 +0200

    deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7a5db768bfdc6b0f3f2a50a8fda9ad554f1fc720',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7a5db768bfdc6b0f3f2a50a8fda9ad554f1fc720',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v220',
+            'version' => '220.0.0.0',
+            'reference' => 'af0935921c10dc7d2d2f41b06c0a66c89102d8dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
